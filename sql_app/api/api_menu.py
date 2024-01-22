d9b37d70851048fdb9367a419928e1c5b166056a
nnnhurt <nnnzagora625@mail.ru>
2024-01-22 21:01:02 +0300

    2

--- sql_app/api/api_menu.py
+++ sql_app/api/api_menu.py
@@ -16,8 +16,8 @@ def read_menu(menu_id: UUID, db: Session = Depends(get_db)):
     if db_menu is None:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
                             detail="menu not found")
-    db_menu.submenus_count = crud.submenu_count(db, menu_id=menu_id)
-    db_menu.dishes_count = crud.dish_count(db, menu_id=menu_id)
+    db_menu.submenu_count_in_menu = crud.submenu_count(db, menu_id=menu_id)
+    db_menu.dish_count_in_menu = crud.dish_count(db, menu_id=menu_id)
     return db_menu
 
 
@@ -25,18 +25,20 @@ def read_menu(menu_id: UUID, db: Session = Depends(get_db)):
 def read_all_menus(db: Session = Depends(get_db)):
     menus = crud.get_all_menu(db)
     for row in menus:
-        row.submenu_count = crud.submenu_count_in_menu(db, menu_id=row.id)
-        row.dishes_count = crud.dish_count_in_menu(db, menu_id=row.id)
-        return menus
+        row.submenu_count_in_menu = crud.submenu_count_in_menu(
+            db, menu_id=row.id)
+        row.dish_count_in_menu = crud.dish_count_in_menu(db, menu_id=row.id)
+    return menus
 
 
 @router.post("/menus", response_model=schemas.Menu, status_code=HTTPStatus.CREATED)
 def create_menu(menu: schemas.CreateMenu, db: Session = Depends(get_db)):
     db_menu = crud.create_menu(db=db, menu=menu)
-    db_menu.submenu_count = (
+    db_menu.submenu_count_in_menu = (
         crud.submenu_count_in_menu(db, menu_id=db_menu.id)
     )
-    db_menu.dish_count = crud.dish_count_in_menu(db, menu_id=db_menu.id)
+    db_menu.dish_count_in_menu = crud.dish_count_in_menu(
+        db, menu_id=db_menu.id)
     return db_menu
 
 
@@ -47,8 +49,9 @@ def update_menu(menu_id: UUID, menu: schemas.Menu, db: Session = Depends(get_db)
         raise HTTPException(
             status_code=HTTPException.NOT_FOUND, detail="menu not found")
     db_menu = crud.update_menu(db=db, menu_id=menu_id, menu=menu)
-    db_menu.submenu_count = (crud.submenu_count_in_menu(db, menu_id=menu_id))
-    db_menu.dish_count = crud.dish_count_in_menu(db, menu_id=menu_id)
+    db_menu.submenu_count_in_menu = (
+        crud.submenu_count_in_menu(db, menu_id=menu_id))
+    db_menu.dish_count_in_menu = crud.dish_count_in_menu(db, menu_id=menu_id)
     return db_menu
 
 
